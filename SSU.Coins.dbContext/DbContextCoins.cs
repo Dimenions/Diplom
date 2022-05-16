75fc8f15969fc9879bf8e3d84aaf8a47e484c5cb
Dmitriy <dimenions@mail.ru>
2022-05-16 16:59:46 +0400

    Миграции

--- SSU.Coins.dbContext/DbContextCoins.cs
+++ SSU.Coins.dbContext/DbContextCoins.cs
@@ -8,11 +8,11 @@ public class CoinsContext : DbContext
     public DbSet<Country> Countries { get; set; }
     public DbSet<Coin> Coins { get; set; }
 
-    public CoinsContext(DbContextOptions<CoinsContext> options)
+    /*public CoinsContext(DbContextOptions<CoinsContext> options)
         : base(options)
     {
         Database.EnsureCreated();
-    }
+    }*/
 
     /*public CoinsContext()
     {
@@ -21,13 +21,16 @@ public class CoinsContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
     }
-
+    */
+    //Add-Migration Initial
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
- {
+    {
         if (!optionsBuilder.IsConfigured)
         {
-            optionsBuilder.UseSqlServer("ConnectionString");
+            //optionsBuilder.UseSqlServer("ConnectionString");
+            optionsBuilder.UseSqlServer(@"Server = (LocalDb)\MSSQLLocalDB; initial catalog = SSU.Coins.dbContext.SSU.Coin; integrated security = True; MultipleActiveResultSets = True; App = EntityFramework");
+            //Первый варик нужно вернуть, но хз как это затестить 
         }
- }*/
+    }
 
 }
