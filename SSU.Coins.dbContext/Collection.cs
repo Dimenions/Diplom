6936feb6dcfcecfe450ffdaac2fb3e786c98e837
Dmitriy <dimenions@mail.ru>
2022-05-16 19:27:57 +0400

    add class

---
+++ SSU.Coins.dbContext/Collection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSU.Coins.dbContext
+{
+    public class Collection
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int[] CoinId { get; set; }
+
+        [MaxLength(20)]
+        public double SaleCoin { get; set; }
+
+        [MaxLength(1000000)]
+        public string Information{ get; set; }
+
+        public byte[] Picture { get; set; }
+    }
+}
---
+++ SSU.Coins.dbContext/Sale.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSU.Coins.dbContext
+{
+    public class Sale
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int CoinId { get; set; }
+
+        [MaxLength(20)]
+        public double SaleCoin { get; set; }
+
+        [MaxLength(1000000)]
+        public string Information { get; set; }
+
+        public bool Activ { get; set; }
+
+        public byte[] Picture { get; set; }
+    }
+}
